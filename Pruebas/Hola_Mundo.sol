a9806cc9df775da37e70501a9143d2c7910560d5
Athernix <hanichiro8@gmail.com>
2024-09-09 19:50:28 -0500

    Hola_Mundo

---
+++ Pruebas/Hola_Mundo.sol
@@ -0,0 +1,6 @@
+// Mi primer contrato Hola mundo
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+contract Hola_Mundo {
+    string public saludo = "Hola mundo desde avalanche Colombia";
+}
